a082df6694d11ea9120eb0f086be7e4839ae2e27
and-jonas <jonas.anderegg@usys.ethz.ch>
2019-06-18 18:37:26 +0200

    Cubist Parallelized

--- R/Analysis/06_multivariate_perf.R
+++ R/Analysis/06_multivariate_perf.R
@@ -28,20 +28,20 @@ dirto <- NULL
 
 #Prepare data ----
 
-
 data <- readRDS(paste(dirfrom, "smth_avg_rflt_bin6_scrunscaled.rds", sep = ""))
 
 # Experiments
 Exp <- c("FPWW012", "FPWW018", "FPWW022")
 
 # create train list of length 7
-train <- list(Exp[1]
-              # ,Exp[2], Exp[3], Exp[1:2], Exp[2:3], Exp[c(1, 3)], Exp[1:3]
+train <- list(Exp[1], Exp[2]
+              # , Exp[3], Exp[1:2], Exp[2:3], Exp[c(1, 3)], Exp[1:3]
               )
 
 # create validation list of length 7
-test <- list(c(list(Exp[2], Exp[3], Exp[2:3]))
-             # , c(list(Exp[1], Exp[3], Exp[c(1, 3)])),
+test <- list(c(list(Exp[2], Exp[3], Exp[2:3])),
+             c(list(Exp[1], Exp[3], Exp[c(1, 3)]))
+             # , 
              # c(list(Exp[1], Exp[2], Exp[1:2])),
              # c(list(Exp[3])),
              # c(list(Exp[1])),
@@ -49,38 +49,42 @@ test <- list(c(list(Exp[2], Exp[3], Exp[2:3]))
              # c(list(Exp[1], Exp[2], Exp[3]))
              )
 
-tuneGrid <- expand.grid(committees = c(1, 2, 5, 10, 20, 50),
+tuneGrid <- expand.grid(committees = c(1, 2, 5, 10, 20),
                         neighbors = 0)
 
 #====================================================================================== -
 
 #Run sequentially ----
 
-
-result <- eval_multivariate_cross(Trait = "SnsCnp", method = "cubist", tuneGrid = 10,
-                                  train = train, test = test, data = data, 
+tic()
+result <- eval_multivariate_cross(Trait = "SnsCnp", method = "pls", 
+                                  tuneGrid = NULL, maxcomp = 15,
+                                  train = train, test = test, data = data,
                                   trainsample = "fullsample", 
-                                  preProc = "center_scale")
-
+                                  testsample = "upsample",
+                                  preProc = "center_scale", runParallel = FALSE)
+toc()
 #====================================================================================== -
 
 #Run in parallel ----
 
-
-cluster <- makeCluster(12, outfile = "") # convention to leave 1 core for OS
-registerDoParallel(cluster)
-clusterEvalQ(cluster, library(doParallel))
-clusterEvalQ(cluster, .libPaths("T:/R3UserLibs_rds"))
-clusterEvalQ(cluster, library(doParallel))
-
 tic()
-result <- eval_multivariate_cross(Trait = "SnsCnp", method = "cubist", tuneGrid = 10,
+result <- eval_multivariate_cross(Trait = "SnsCnp", method = "cubist", 
+                                  tuneGrid = tuneGrid, maxcomp = NULL,
                                   train = train, test = test, data = data, 
                                   trainsample = "fullsample", 
-                                  preProc = "center_scale")
+                                  testsample = "upsample",
+                                  preProc = "center_scale", 
+                                  runParallel = TRUE)
 toc()
 
-stopCluster(cluster)
-registerDoSEQ()
-
 #====================================================================================== -
+
+#Run all in parallel ----
+##Create a grid with all possible argument combinations
+args1 <- expand.grid(Trait = "SnsCnp",
+                     method = c("pls", "cubist"), 
+                     preProcess = "center_scale",
+                     maxcomp = 25, 
+                     dir = "T:/PhD/DataAnalysis/FPWW012_FPWW018/Correlation Analyses/Correlation_PLSR/Data/",
+                     subset = c("FPWW012", "FPWW018"))
--- R/Utils/003_multivariate_utils.R
+++ R/Utils/003_multivariate_utils.R
@@ -1,22 +1,34 @@
 
+#Helper functions
+########################################################################################
+
 #calculate RMSE
 get_rmse = function(actual, predicted) {
   sqrt(mean((actual - predicted) ^ 2))
 }
 
-#calculate RMSE on upsampled validation dataset
-get_RMSE_upsample <- function(data){
+#perform up- or downsampling
+perform_sampling <- function(data, method = "upsampling"){
   data[, Trait] <- as.factor(data[, Trait])
-  data <- upSample(x = data,
-                   y = data[, Trait],
-                   yname = Trait)
+  if(method == "upsampling"){
+    data <- upSample(x = data,
+                     y = data[, Trait],
+                     yname = Trait)
+  } else if (method == "downsample"){
+    data <- downSample(x = data,
+                       y = data[, Trait],
+                       yname = Trait)
+  } else print("specify sampling procedure for test data")
   data[, Trait] <- as.numeric(as.character(data[, Trait]))
   data <- data[, -ncol(data)]
-  #count samples used for validation
-  nsamp <- nrow(data)
+  return(data)
+}
+
+#calculate RMSE on upsampled validation dataset
+get_RMSE_upsample <- function(data){
+  data <- perform_sampling(data, method = "upsampling")
   ID_sub <- data %>% 
     dplyr::select(-contains("rflt"), -SnsCnp, -SnsFl0)
-  #remove IDs from validation data
   data_sub <- data %>% 
     select(contains("rflt"), SnsCnp, -SnsFl0)
   #predict new data using trained model
@@ -24,26 +36,46 @@ get_RMSE_upsample <- function(data){
                                type = "raw", na.action = na.omit)
   predobs <- cbind(ID_sub, data[, Trait], pred) %>% as_tibble()
   names(predobs)[length(predobs)-1] <- "obs"
-  RMSE <- round(rmse(predobs$obs, predobs$pred), 2)
+  RMSE <- round(get_rmse(predobs$obs, predobs$pred), 2)
   return(RMSE)
 }
+
+#generate predictions and extract corresponding real observations
+extract_predobs <- function(data){
+  ID <- data %>% 
+    select(-contains("rflt"), -SnsCnp, -SnsFl0)
+  dat <- data %>% 
+    select(contains("rflt"), SnsCnp, -SnsFl0)
+  #create predictions
+  pred <- caret::predict.train(fit, newdata = dat, 
+                               type = "raw", na.action = na.omit)
+  #create output tibble
+  predobs <- cbind(ID, dat[, Trait], pred) %>% as_tibble()
+  names(predobs)[length(predobs)-1] <- "obs"
+  return(predobs)
+}
+
+
+#Main function
+########################################################################################
 
 #evaluate model performance within and across years
 eval_multivariate_cross <- function(Trait, method = "pls",
                                     maxcomp = NULL, tuneGrid = NULL, 
                                     data, train, test, 
-                                    trainsample, 
-                                    preProc, 
-                                    bdir){
+                                    trainsample = "fullsample", 
+                                    testsample = "upsample",
+                                    preProc = "center_scale", 
+                                    runParallel = FALSE){
   
-  #train:       A list of training datasets. Each list element consists of a vector with the year(s) used for training
-  #test:        A list of validation sets corresponding to ONE training set; 
-  #             Each list element consists of a list of vectors; 
-  #             Vectors contain all possible combinations of validation data sets. 
-  #data:        A list of dataframes, each list element contains all the data of a particular type. 
-  #trainsample: Procedure to assure class balance in the training set. Either "downsampling" or "fullsample".
-  #preProcess:  Preprocessing of spectral data: Either "none", "center", "scale", or "center_scale"
-  #bdir:        Sink directory for output
+  #set up a cluster
+  if(runParallel){
+    cluster <- makeCluster(12, outfile = "")
+    registerDoParallel(cluster)
+    clusterEvalQ(cluster, library(MASS))
+    clusterEvalQ(cluster, library(caret))
+    clusterEvalQ(cluster, library(tidyverse))
+  }
   
   #for each of the possible training datasets
   #i.e. for each year and all combinations of years
@@ -54,22 +86,17 @@ eval_multivariate_cross <- function(Trait, method = "pls",
     print(paste("> train on", paste(train[[i]], collapse = "_")))
     
     #====================================================================================== -
-    
-    # SELECT TRAINING DATA
+    #TRAIN ALGORITHM, VALIDATE ON THE SAME YEAR#
+    #====================================================================================== -
     
     #extract data for training year(s)
     dat_train0 <- data %>% filter(Exp %in% train[[i]]) %>% as.data.frame()
     
-    #select training data:
-    ##sample data if required
+    #sample data if required
     if(trainsample == "downsample"){
-      dat_train <- dat_train0
-      dat_train[, Trait] <- dat_train[, Trait] %>% as.factor()
-      dat_train <- downSample(x = dat_train,
-                              y = dat_train[, Trait],
-                              yname = Trait)
-      dat_train[, Trait] <- as.numeric(as.character(dat_train[, Trait]))
-      dat_train <- dat_train[,-ncol(dat_train)]
+      dat_train <- perform_sampling(dat_train0, method = "downsampling")
+    } else if (trainsample == "upsample"){
+      dat_train <- perform_sampling(dat_train0, method = "upsampling")
     } else if(trainsample == "fullsample") {
       dat_train <- dat_train0
     } else print("specify sampling procedure for train data")
@@ -84,10 +111,6 @@ eval_multivariate_cross <- function(Trait, method = "pls",
     #keep only predictors
     dat <- dat_train %>% 
       select(Trait, contains("rflt"))
-    
-    #====================================================================================== -
-    
-    # TRAIN ALGORITHM
     
     if(preProc == "none"){
       preProcess <- NULL
@@ -106,7 +129,6 @@ eval_multivariate_cross <- function(Trait, method = "pls",
                                 selectionFunction = "oneSE",
                                 allowParallel = TRUE,
                                 verboseIter = TRUE)
-    
     if(method == "pls"){
       fit <- caret::train(as.formula(paste(Trait, "~.", sep = "")),
                           data = dat, 
@@ -116,8 +138,6 @@ eval_multivariate_cross <- function(Trait, method = "pls",
                           trControl = ctrl,
                           importance = TRUE)
     } else if(method == "cubist"){
-      tuneGrid <- expand.grid(committees = c(1, 2, 5, 10, 20),
-                              neighbors = 0)
       fit <- caret::train(as.formula(paste(Trait, "~.", sep = "")),
                           data = dat, 
                           preProcess = preProcess,
@@ -126,46 +146,42 @@ eval_multivariate_cross <- function(Trait, method = "pls",
                           trControl = ctrl,
                           importance = TRUE)
     }
-    
+
     #====================================================================================== -
-    
-    # PREDOBS FOR TRAINING SAMPLES
-    
+
+    #extract predobs and train RMSE
     predobs <- plyr::match_df(fit$pred, fit$bestTune, on = names(fit$bestTune)) %>% 
       group_by(rowIndex) %>%
       summarize(obs = mean(obs),
                 mean_pred = mean(pred))
-    
-    #add observation ID
     ID_train$rowIndex <- 1:nrow(ID_train)
     predobs <- dplyr::full_join(ID_train, predobs, by = "rowIndex") %>% 
       dplyr::select(-rowIndex) %>% as_tibble()
-    
     RMSE_train <- caret::getTrainPerf(fit) %>% pull(TrainRMSE)
-    RMSE_train_up <- replicate(10, get_RMSE_upsample(dat_train0)) %>% mean()
     
-    #====================================================================================== -
+    #calcualte RMSE for upsamples
+    if(isTRUE(runParallel)){
+      clusterExport(cluster, c("dat_train0", "get_RMSE_upsample", "perform_sampling", 
+                               "get_rmse", "Trait", "fit"), 
+                    envir=environment())
+      RMSE_train_up <- parSapply(cluster, 1:10, function(i, ...) {x <- get_RMSE_upsample(dat_train0)}) %>% 
+        mean() %>% round(., 2)
+    } else {
+      RMSE_train_up <- replicate(10, get_RMSE_upsample(dat_train0)) %>% mean()
+    }
+    print("==train RMSE calcualted==")
+
+    #create predictions for all samples in the training dataset if required;
+    #extract corresponding real observations
+    if(trainsample == "downsample"){
+      predobs_all <- extract_predobs(dat_train0)
+    } else {
+      predobs_all <- predobs
+    }
     
-    # PREDOBS FOR ALL SAMPLES OF TRAINING EXP
-    
-    #extract observation IDs
-    ID_train0 <- dat_train0 %>% 
-      select(-contains("rflt_"), -SnsCnp, -SnsFl0)
-    
-    #predict new data using trained model
-    pred <- caret::predict.train(fit, newdata = dat_train0, 
-                                 type = "raw", na.action = na.omit)
-    
-    predobs_all <- cbind(ID_train0, dat_train0[, Trait], pred) %>% as_tibble()
-    names(predobs_all)[length(predobs_all)-1] <- "obs"
-    
-    #====================================================================================== -
-    
-    # CREATE OUTPUT OF TRAINING
-    
+    #create output tibble
     out_train[[i]] <-  tibble(Trait = Trait, 
                               train = paste(train[[i]], collapse = "_"),
-                              datatype = k,
                               trainsample = trainsample,
                               method = method, 
                               tunepar = list(fit$bestTune[1, 1]), 
@@ -173,79 +189,71 @@ eval_multivariate_cross <- function(Trait, method = "pls",
                               RMSE_train = RMSE_train,
                               RMSE_train_up = RMSE_train_up,
                               nsamp_train = nsamp,
-                              predobs_train_all = list(predobs_all)
-                              )
+                              predobs_train_all = list(predobs_all))
     
     #====================================================================================== -
-    #TRAINING AND VALIDATION ON THE SAME YEAR COMPLETE#
+    #VALIDATION ON DIFFERENT EXPERIMENT(S)#
     #====================================================================================== -
     
-    # Model validation on validation data: different Experiment(s)
-    
-    #create empty list to take up predobs data
     out_test <- list()
-    
     #for each validation dataset
     for(j in 1:length(test[[i]])){
       
-      #extractcorresponding data from the complete dataset
+      #get data
       dat_test0 <- data %>% 
         filter(Exp %in% test[[i]][[j]]) %>% as.data.frame()
       
       #count samples used for validation
       nsamp_all <- nrow(dat_test0)
       
-      ID_test_sub <- dat_test0 %>% 
-        select(-contains("rflt"), -SnsCnp, -SnsFl0)
+      #create predictions,
+      #extract corresponding real observations
+      predobs <- extract_predobs(dat_test0)
       
-      #remove IDs from validation data
-      dat_test_sub <- dat_test0 %>% 
-        select(contains("rflt"), SnsCnp, -SnsFl0)
+      #calculate RMSE on upsamples
+      #or on fullsamples
+      if(isTRUE(runParallel)){
+        clusterExport(cluster, c("dat_test0"), envir=environment())
+        RMSE_test_up <- parSapply(cluster, 1:10, function(i, ...) {x <- get_RMSE_upsample(dat_test0)}) %>%
+          mean() %>% round(., 2)
+      } else {
+        RMSE_test_up <- replicate(10, get_RMSE_upsample(dat_test0)) %>% 
+        mean() %>% round(., 2)
+      }
+      predobs_all <- extract_predobs(dat_test0)
+      RMSE_test <- round(get_rmse(predobs_all$obs, predobs_all$pred), 2)
+      print(paste("==test RMSE calculated,", "(", j, ")", "==", sep = ""))
       
-      #predict new data using trained model
-      pred <- caret::predict.train(fit, newdata = dat_test_sub, 
-                                   type = "raw", na.action = na.omit)
-      
-      predobs <- cbind(ID_test_sub, dat_test0[, Trait], pred) %>% as_tibble()
-      names(predobs)[length(predobs)-1] <- "obs"
-      
-      RMSE_test_up <- replicate(10, get_RMSE_upsample(dat_test0)) %>% mean()
-      
-      #######################################################################
-      #extract predictions for all observations in validation year
-      #######################################################################
-      
-      #extract observation IDs
-      ID_test0 <- dat_test0 %>% 
-        select(-contains("rflt"), -SnsCnp, -SnsFl0)
-      
-      #predict new data using trained model
-      pred <- caret::predict.train(fit, newdata = dat_test0, 
-                                   type = "raw", na.action = na.omit)
-      
-      predobs_all <- cbind(ID_test0, dat_test0[, Trait], pred) %>% as_tibble()
-      names(predobs_all)[length(predobs_all)-1] <- "obs"
-      
-      RMSE_all <- round(rmse(predobs_all$obs, predobs_all$pred), 2)
-  
-      #create a list containing all the output for the training dataset and the internal validation
-      
+      #create output tibble
       out_test[[j]] <-  tibble(test = paste(test[[i]][[j]], collapse = "_"),
-                               RMSE_test = RMSE_all,
+                               RMSE_test = RMSE_test,
                                RMSE_test_up = RMSE_test_up,
                                nsamp_test = nsamp,
-                               predobs_test_all = list(predobs_all)
-                               )
-                               
-    } #end of validation sets loop
-    
-    #combine the training and validation results
+                               predobs_test_all = list(predobs_all))
+    }#end of validation loop
     out_test_done <- bind_rows(out_test)
     out_all[[i]] <- tibble(train = list(out_train[[i]]), test = list(out_test_done)) %>% 
       unnest(train) %>% unnest(test, .preserve = c(predobs_train_all, tunepar))
-  
   }#end of training set loop
-  
   out <- bind_rows(out_all)
-    
-} #end of function
+  if(isTRUE(runParallel)){
+    stopCluster(cluster)
+    registerDoSEQ()
+  }
+  return(out)
+}#end of function
+
+
+#train:       A list of training datasets; 
+#             Each list element consists of a vector with the Exp(s) used for training
+#test:        A list of validation sets corresponding to ONE training set; 
+#             Each list element consists of a list of vectors; 
+#             Vectors contain all possible combinations of validation data sets. 
+#trainsample: Procedure to assure class balance in the training set if required; 
+#             Either "downsampling" or "fullsample".
+#             It is strongly recommended to use "fullsample";
+#             Downsampling negatively affects model performance; 
+#             Upsampling results in a large increase in computation time. 
+#preProcess:  Preprocessing of spectral data;
+#             Either "none", "center", "scale", or "center_scale".
+
